cfedd44f584db0ecc2c3bf96c6d7428276fd885e
Tky567 <caonhan1950@gmail.com>
2025-07-02 19:37:24 +0700

    Update setupbot.sh

--- setupbot.sh
+++ setupbot.sh
@@ -8,6 +8,6 @@ pkg install python git -y
 pip install -U pip
 pip install -U discord.py
 pip install python-dotenv
-wget -o /storage/emulated/0/Download/discord-bot.zip https://github.com/Tky567/setupvps/raw/refs/heads/main/discord-bot.zip
+curl -o discord-bot.zip https://github.com/Tky567/setupvps/raw/refs/heads/main/discord-bot.zip
 unzip discord-bot.zip
 cd /sdcard/Download/discord-bot && python free.py
