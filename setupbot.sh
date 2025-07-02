20c5687d212d43755a6440143399b9a9ba184ff2
Tky567 <caonhan1950@gmail.com>
2025-07-02 10:24:58 +0700

    Update setupbot.sh

--- setupbot.sh
+++ setupbot.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-termux-setup-storage && yes
-pkg update && yes
-pkg upgrade && yes
-pkg install wget && yes
+termux-setup-storage -y
+pkg update -y
+pkg upgrade -y
+pkg install wget -y
 pkg install python git
 pip install -U pip
 pip install -U discord.py
 pip install python-dotenv
-
-cd /sdcard/Download/ && python bot.py
+wget -o /storage/emulated/0/Download/discord-bot.zip https://github.com/Tky567/setupvps/raw/refs/heads/main/discord-bot.zip
+unzip discord-bot.zip
+cd /sdcard/Download/discord-bot && python free.py
