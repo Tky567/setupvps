1b1fb5a4a0845e743c8b3945aaee72682c2d68af
Tky567 <caonhan1950@gmail.com>
2025-07-03 14:29:26 +0700

    Update host.sh

--- host.sh
+++ host.sh
@@ -1,10 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
-pip install -U discord.py python-dotenv requests
-
-wget -c https://raw.githubusercontent.com/Tky567/setupvps/refs/heads/tky/free.py
-
-wget -c https://gist.githubusercontent.com/Tky567/72e45ee3dfb3069e27ff4301551515aa/raw/ae94db72e20e0b2cda5b2b957bd32aba1ce33e73/config.env
-
-cd discord-bot
-python free.py
+npm install dotenv
+wget -c https://gist.githubusercontent.com/Tky567/42a8449ecce80ddc956592f22824ab6b/raw/a5958bd99dab5e32639b83c4098053b040a89b9a/package.json
+wget -c https://gist.githubusercontent.com/Tky567/13966fe1ab2557e4303da585b45e8e4a/raw/403be5e76f806c3a4fc353695f97f0a8e689dbe1/index.js
+node index.js
