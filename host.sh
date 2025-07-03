0deece7a47f34154e4205bb471ba333ec22a3d3c
Tky567 <caonhan1950@gmail.com>
2025-07-03 11:23:58 +0700

    Update host.sh

--- host.sh
+++ host.sh
@@ -2,9 +2,7 @@
 
 pip install -U discord.py python-dotenv requests
 
-wget -c https://github.com/Tky567/setupvps/raw/refs/heads/tky/discord-bot.zip -O discord-bot.zip
-
-unzip -o discord-bot.zip
+wget -c https://github.com/Tky567/setupvps/raw/refs/heads/tky/discord-bot.zip -O discord-bot.
 
 wget -c https://gist.githubusercontent.com/Tky567/72e45ee3dfb3069e27ff4301551515aa/raw/ae94db72e20e0b2cda5b2b957bd32aba1ce33e73/config.env
 
