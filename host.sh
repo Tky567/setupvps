125be2e17e21a313768832d48d739fbdae990148
Tky567 <caonhan1950@gmail.com>
2025-07-04 23:55:34 +0700

    Update host.sh

--- host.sh
+++ host.sh
@@ -2,4 +2,5 @@
 npm install discord.js
 npm install dotenv
 wget -c https://gist.githubusercontent.com/Tky567/13966fe1ab2557e4303da585b45e8e4a/raw/403be5e76f806c3a4fc353695f97f0a8e689dbe1/index.js
+wget -c https://gist.githubusercontent.com/Tky567/42a8449ecce80ddc956592f22824ab6b/raw/a5958bd99dab5e32639b83c4098053b040a89b9a/package.json
 node index.js
